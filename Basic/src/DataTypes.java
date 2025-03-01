0536b2e18cbe577ad79e9a470563f5dbedaa130d
Donildison Afonso <donidilsonafonso496@gmail.com>
2025-03-01 17:50:46 +0000

    First commit

---
+++ Basic/src/DataTypes.java
@@ -0,0 +1,8 @@
+// Date: 01-03-2025
+// Description: Data types in java
+
+public class DataTypes {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Basic/src/Output.java
@@ -0,0 +1,30 @@
+// Date: 01-03-2025
+// Description: Output in java
+
+// The class name has to match the name of the file
+
+public class Output {
+    public static void main(String[] args) {
+
+        // Print a message with println
+        // This does not add a line break
+        System.out.println("Hello, World!");
+        System.out.println("This is my first message in Java.");
+
+        // Use print
+        // This adds a line break
+        System.out.print("\nHello world. ");
+        System.out.print("Print does not add a line break");
+
+
+        // This is a single line comment
+
+        /*
+            This
+            is
+            a
+            multiline
+            comment
+        */
+    }
+}
